e0ae3fb38eccdaf6a4ebe238c5a8206107481d33
Nicole Payne <nicole1rock@gmail.com>
2023-06-07 21:33:37 -0500

    bmi Calculator

--- calculator.js
+++ calculator.js
@@ -19,7 +19,23 @@ app.post("/", function(req,res){
     let result = num1 + num2;
 
     res.send("The result of the calculation is " + result);
-})
+});
+
+// For BMI Calculator
+
+app.get("/bmiCalculator", function(req,res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", function(req,res){
+       
+    let weight = parseFloat(req.body.wt);
+    let height = parseFloat(req.body.ht);
+    let bmi = Math.round(weight / Math.pow(height,2));
+    
+    res.send("Your BMI is " + bmi);
+    
+});
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000.");
