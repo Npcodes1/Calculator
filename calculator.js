f393a23cf0b053bd12ab736e869427d26358aeb3
Nicole Payne <nicole1rock@gmail.com>
2023-06-07 19:48:05 -0500

    calculator

---
+++ calculator.js
@@ -0,0 +1,13 @@
+// js hint esversion:6
+
+const express = require ("express"); 
+
+const app = express();
+
+app.get("/", function(req, res) {
+    res.send("Hello World!")
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000.");
+});
